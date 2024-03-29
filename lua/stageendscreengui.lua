c925dc0cc462d2eef1520fd6e516529e3247ae15
vojin154 <vojin154@gmail.com>
2024-03-29 14:17:15 +0100

    Fixed getting wrong table value

--- lua/stageendscreengui.lua
+++ lua/stageendscreengui.lua
@@ -22,5 +22,5 @@ end)
 
 local values = MissedShotsTracker.values
 Hooks:PreHook(StageEndScreenGui, "feed_statistics", "feed_statistics_missed_shots_tracker", function(self, data)
-   data.missed_shots = values.shots_missed_new
+   data.missed_shots = values.shots_missed
 end)
